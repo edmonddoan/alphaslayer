3574ab93b048739d0aa4e8a9920c8e6ec0b74b14
Edmond Doan <edmonddoan@gmail.com>
2022-07-03 17:40:55 -0500

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Navbar from "./components/navbar"
+import Dogs from "./pages/Dogs"
+import Cats from "./pages/Cats"
+import Sheeps from "./pages/Sheeps"
+import Goats from "./pages/Goats"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Dogs} />
+        <Route path='/cats' component={Cats} />
+        <Route path='/sheeps' component={Sheeps} />
+        <Route path='/goats' component={Goats} />
+      </Routes>
+    </Router>
   );
 }
-
+  
 export default App;
